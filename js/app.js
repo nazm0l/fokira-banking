d90e28a7c27cd671745f0ae9e062719ccfb07717
nazm0l <tanjirahmednazmul@gmail.com>
2022-02-10 01:26:31 +0600

    dashboard designed

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ document.getElementById("submit-btn").addEventListener("click", function () {
   // user Email
   const emailField = document.getElementById("user-email");
   const userEmail = emailField.value;
-  console.log(userEmail);
   // user Password
   const passwordField = document.getElementById("user-password");
   const userPassword = passwordField.value;
-  console.log(userPassword);
 
   if (userEmail == "web@nazmul.com" && userPassword == "nazmul") {
     window.location.href = "banking.html";
+  } else {
+    alert("please enter valid info");
   }
 });
